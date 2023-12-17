f4299e13fa03dd87d24447cf16016baee160d93b
ajit250103 <ajit.pal.2501@gmail.com>
2023-12-18 04:05:58 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+    <div>
+        <h1>TechyStar</h1>
+        <p>@all right reserved</p>
+    </div>
+
+    <div>
+       <h5>Follow Us</h5> 
+       <div>
+        <a href="https://www.linkedin.com/in/ajit-kumar-pal-48a64221a" target={"blank"}>
+        LinkedIn</a>
+        <a href="https://github.com/ajit250103" target={"blank"}>
+        Github</a>
+        <a href="https://www.instagram.com/ajit.pal.25/" target={"blank"}>
+        Instagram</a>
+       </div>
+    </div>
+  </footer>
+
+  );  
+};
+
+export default Footer
---
+++ src/components/Home.jsx
@@ -0,0 +1,92 @@
+import React from 'react' ;
+import vg from "../assets/2.webp";
+import {AiFillGoogleCircle,
+     AiFillAmazonCircle,
+     AiFillYoutube,
+     AiFillInstagram} 
+    from "react-icons/ai";
+
+const Home = () => {
+  return (
+
+    <>
+        <div className="home" id="home">
+        <main>
+            <h1>TechyStar</h1>
+            <p>Solution to all your problems.</p>
+        </main>
+        </div>
+
+        <div className="home2">
+            <img src={vg} alt="Graphics" />
+            <div>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Recusandae quasi cupiditate, asperiores sed voluptates 
+                    ipsa doloribus dignissimos quo. Labore beatae dolore amet illum 
+                    consequatur nisi repellendus quis est neque quod.
+                </p>
+            </div>
+        </div>
+
+
+        <div className="home3" id='about'>
+            <div>
+                <h1>Who we are?</h1>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing
+                    elit. Nemo ea labore, voluptatem est vitae, harum 
+                    minus vero, atque eveniet sapiente saepe. Pariatur
+                    optio dolorem quam inventore odit quo quia perferendis?
+                </p>
+            </div>
+        </div>
+
+        <div className="home4" id='brands'>
+
+            <div>
+                <h1>Brands</h1>
+
+                <article>
+                    <div
+                    style={{
+                        animationDelay: "0.3s",
+                    }}>
+                       <AiFillGoogleCircle />
+                       <p>Google</p> 
+
+                    </div>
+                    <div
+                    style={{
+                        animationDelay: "0.5s",
+                    }}>
+                       <AiFillAmazonCircle />
+                       <p>Amazon</p> 
+
+                    </div>
+
+                    <div
+                    style={{
+                        animationDelay: "0.7s",
+                    }}>
+                       <AiFillYoutube />
+                       <p>Youtube</p> 
+
+                    </div>
+
+                    <div
+                    style={{
+                        animationDelay: "1s",
+                    }}>
+                       <AiFillInstagram />
+                       <p>Instagram</p> 
+
+                    </div>
+                </article>
+            </div>
+        </div>
+    </>
+  );
+};
+
+export default Home;
